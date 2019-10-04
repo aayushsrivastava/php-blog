e4a960a917e9922174174a0e4994c81a1ab9918d
Aayush Srivastava <37486345+aayushsrivastava@users.noreply.github.com>
2019-10-05 01:44:10 +0530

    add logout functionality

--- views/_welcome_user.php
+++ views/_welcome_user.php
@@ -1,7 +1,9 @@
+|
 <span>Welcome
 <?php
 $user = new User();
 $session_user = $user->read($_SESSION['user']);
 echo $session_user['first_name'] . " " . $session_user['last_name'];
-?>
+?> 
+<a href="/logout.php">(Sign out)</a>
 </span>
