0737480bd8dfeded942c1172d3c9461e002cd4d9
Aayush Srivastava <37486345+aayushsrivastava@users.noreply.github.com>
2019-10-05 23:35:37 +0530

    add comment author's profile link

--- views/_article.php
+++ views/_article.php
@@ -33,10 +33,12 @@ foreach($article_comments as $comment) {
     $comment_content = $comment['comment'];
     $comment_date = $comment['creation_date'];
     $comment_edit = $comment['last_edit_date'];
+    $comment_author_id = $comment['userID'];
 
     echo "<div class=\"comment-box\">\n" . 
-        "\t<div class=\"comment-author\">$comment_author</div>\n" .
-        "\t<div class=\"comment-dates\">\n\t\t". 
+        "\t<div class=\"comment-author\">" .
+        "<a href=\"/user.php?id=$comment_author_id\">$comment_author</a>" .
+        "</div>\n\t<div class=\"comment-dates\">\n\t\t". 
         "Created on: <span class=\"comment-created\">$comment_date</span>\n" .
         "\t\tLast edit: <span class=\"comment-edited\">$comment_edit</span>" .
         "\n\t</div>\n\t<div class=\"comment-content\">\n";
