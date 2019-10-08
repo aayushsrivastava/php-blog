0b22e0ea07c4042c06fb814a8fd961b91264da33
Aayush Srivastava <37486345+aayushsrivastava@users.noreply.github.com>
2019-10-08 15:48:59 +0530

    interface change for user profile

--- controllers/user.php
+++ controllers/user.php
@@ -1,6 +1,17 @@
 <?php
+session_start();
+
 include('../models/import.php');
 $user = new User();
+
+$authorized = false;
+if (isset($_SESSION['user'])) {
+    $user_profile = $_GET['id'];
+    if ($user_profile === $_SESSION['user']) {
+        $authorized = true;
+    }
+}
+
 $user_details = $user->read($_GET['id']);
 
 $title = 'User Information';
--- views/_user.php
+++ views/_user.php
@@ -1,5 +1,28 @@
-Name: 
-<?php echo $user_details['first_name'] . " " . $user_details['last_name']; ?>
-<br>
-Email: <?php echo $user_details['email']; ?><br>
-Account created on: <?php echo $user_details['reg_date']; ?>
+<?php
+if ($authorized) {
+    $user_id = $user_details['ID'];
+    echo "<div id=\"user-control\">\n\t<span>" .
+    "<a href=\"/user.php?id=$user_id&amp;action=edit\">Settings</a></span>\n" .
+    "</div>\n";
+}
+?>
+<div id="profile">
+<table>
+<tr>
+    <td>First Name :</td>
+    <td><?php echo $user_details['first_name']; ?></td>
+</tr>
+<tr>
+    <td>Last Name :</td>
+    <td><?php echo $user_details['last_name']; ?></td>
+</tr>
+<tr>
+    <td>Email :</td>
+    <td><?php echo $user_details['email']; ?><br></td>
+</tr>
+<tr>
+    <td>Account created on :</td>
+    <td><?php echo $user_details['reg_date']; ?></td>
+</tr>
+</table>
+</div>
