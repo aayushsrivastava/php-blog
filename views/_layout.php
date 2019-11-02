a86a8621e89da26b1fbdd1d0a3a2bccc2d448616
Aayush Srivastava <37486345+aayushsrivastava@users.noreply.github.com>
2019-11-03 00:18:47 +0530

    add bootstrap

--- views/_layout.php
+++ views/_layout.php
@@ -5,11 +5,16 @@ session_start();
 <html>
 <head>
     <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+<div class="container">
 <?php include('_header.php'); ?>
+</div>
 
+<div class="container">
 <?php include($childView); ?>
+</div>
 
 </body>
 </html>
