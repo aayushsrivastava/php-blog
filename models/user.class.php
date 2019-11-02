028f49c29242cf1421a17cb41ed90e26618c7cc1
Aayush Srivastava <37486345+aayushsrivastava@users.noreply.github.com>
2019-11-02 11:47:28 +0530

    update user details

--- models/user.class.php
+++ models/user.class.php
@@ -41,6 +41,41 @@ class User extends Application {
             'id' => $user_id];
     }
 
+    function validate_password($user_id, $given_password) {
+        $sql = "
+        SELECT password
+        FROM user
+        WHERE user.ID = $user_id";
+
+        $result = $this->conn->query($sql)->fetch_assoc();
+        $stored_password = $result['password'];
+
+        return ($given_password === $stored_password);
+    }
+
+    function update() {
+        if (!$this->validate_password($_GET['id'], $_POST['password'])) {
+            return;
+        }
+
+        $stmt = $this->conn->prepare("
+        UPDATE user
+        SET first_name = ?, last_name = ?, email = ?
+        WHERE user.ID = ?");
+        $stmt->bind_param("sssi", $first_name, $last_name, $email, $user_id);
+
+        $first_name = $_POST['fname'];
+        $last_name = $_POST['lname'];
+        $email = $_POST['email'];
+        $user_id = $_GET['id'];
+
+        if ($stmt->execute()) {
+            return $this->conn->insert_id;
+        } else {
+            return NULL;
+        }
+    }
+
     function delete() {
         $stmt = $this->conn->prepare("
         DELETE FROM user
